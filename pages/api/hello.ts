0f09acbab860accf11246301b4e6673250bc4a66
Aleck Landgraf <aleck.landgraf@airbnb.com>
2020-10-11 20:42:41 -0700

    data fetching

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+type ResponseData = {
+  temp: number;
+  aqi: number;
+  weather: string;
+};
+
+export default (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  res.status(200).json({ temp: 60, aqi: 9, weather: "cloudy" });
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import Head from "next/head";
+import useSWR from "swr";
 import styles from "../styles/Home.module.css";
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Home() {
+  const { data, error } = useSWR("/api/hello", fetcher, {
+    refreshInterval: 60000, // every minute
+  });
+  if (error) {
+    return (
+      <div className={styles.error_container}>
+        <div className={styles.error_content}>
+          There was an error loading the data
+        </div>
+      </div>
+    );
+  }
+  if (!data) {
+    return (
+      <div className={styles.loading_container}>
+        <div className={styles.loading_content}>Loading...</div>
+      </div>
+    );
+  }
   return (
     <div>
       <Head>
         <title>kitchen clock</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
 
       <div className={styles.container}>
         <div className={styles.boxy}>
